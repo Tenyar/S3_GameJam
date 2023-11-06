0aa364b4a396afc2e2c6f327b8999a43350afe36
Arnaud <arnaud.kersuall@gmail.com>
2023-11-06 16:08:26 +0100

    Ajout d'un arret de l'application temporaire

--- Application.py
+++ Application.py
@@ -13,8 +13,11 @@ dt = 0
 
 gameManager = GameManager.GameManager()
 while gameManager.isRunning():
+    # On regarde si l'évenement "quitter la fenêtre" est déclenché.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
     dt = clock.tick(60)
     gameManager.update(dt)
     pygame.display.flip()
-
-pygame.quit()
