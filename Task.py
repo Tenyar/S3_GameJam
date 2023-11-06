85cfbbf2755eedc23959a58247477ea3b1f1405b
BucketMathieu <mathieu.bucket@gmail.com>
2023-11-06 17:04:47 +0100

    task progression

--- Task.py
+++ Task.py
@@ -3,3 +3,6 @@
 class Task:
     def __init__(self) -> None:
         self.completionPercentage = 0
+
+    def addProgress(self, amount : float):
+        self.completionPercentage += amount
--- TaskManager.py
+++ TaskManager.py
@@ -1,5 +1,11 @@
-
+import Task
 
 class TaskManager:
     def __init__(self) -> None:
         self.tasks = []
+
+    def getTaskAmount(self):
+        return len(self.tasks)
+    
+    def progressCurrentTask(self, amount : float):
+        self.tasks[0].addProgress(amount)
