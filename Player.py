73dc0ef4e4b13f7ae54688a1611683e0e435ae49
Arnaud <arnaud.kersuall@gmail.com>
2023-11-06 15:56:06 +0100

    Classe player héritage avec classe Sprite

--- Player.py
+++ Player.py
@@ -1,12 +1,20 @@
-import pygame as pg
+import pygame
 
-
-class Player(pg.sprite):
+class Player(pygame.sprite.Sprite):
 # Déclaration des variables de cette classe.
 
 
-# Constructeur de la classe
-def __init__(self):
+    # Constructeur de la classe
+    # pos_x/pos_y sont les coordonées de ce player(sprite)
+    def __init__(self, width, height):
+        # Initialise l'objet qu'on hérite
+        super().__init__()
+        # Créer une image
+        self.image = pygame.Surface([width,height])
+        # Remplie l'image d'une couleur
+        self.image.fill((255,0,25))
+        # Dessine un rectangle autour de l'image qui prendra comme grandeur la width et height de l'image
+        self.rect = self.image.get_rect()
 
-# Destructeur
-def __del__(self):
+    # Destructeur (rarement utile)
+    # def __del__(self):
