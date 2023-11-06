c950cb220c4771e6d6e186bd6117b5a8836313ac
Arnaud <arnaud.kersuall@gmail.com>
2023-11-06 15:28:06 +0100

    Classe Player vide

---
+++ Player.py
@@ -0,0 +1,12 @@
+import pygame as pg
+
+
+class Player(pg.sprite):
+# Déclaration des variables de cette classe.
+
+
+# Constructeur de la classe
+def __init__(self):
+
+# Destructeur
+def __del__(self):
