1700a171bc776e54a54ddb88eade71b864ab18b2
PaulMaget <paul.pj.maget@gmail.com>
2023-11-06 17:18:51 +0100

    Création de Interactible

---
+++ Interactible.py
@@ -0,0 +1,9 @@
+import pygame
+from pygame.sprite import AbstractGroup
+
+class Interactible(pygame.sprite.Sprite):
+
+    def __init__(self, *groups: AbstractGroup, width, height) -> None:
+        super().__init__(*groups)
+        
+        self.image = pygame.Surface([width,height])
