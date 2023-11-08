2a34f4fdddb3fc5a2452702034f8b90ca9eab9d4
estiennq <quentin.estienne@etu.univ-grenoble-alpes.fr>
2023-11-08 23:53:22 +0100

    inversion parametres SpriteSheet.getat()

--- SpriteSheet.py
+++ SpriteSheet.py
@@ -1,31 +1,24 @@
 import pygame 
 
-class SpriteSheet(object):
+class SpriteSheet():
 
     def __init__(self,filename : str, spritePerLine, spritePerColumn, spriteWidth, spriteHeight):
-        # Chargement de l'image.
-        image = pygame.image.load(filename).convert()
-        # Nombre de sprite totale.
+        image = pygame.image.load(filename)
         NumberOfSprites = spritePerLine*spritePerColumn
-        # Tableau ou sont stocké les sprites.
         self.sprites = {}
-        # autres Attributs de l'instance.
         self.spritePerLine = spritePerLine
         self.spritePerColumn = spritePerColumn
-        # Séparation des sprites et rangement des sprites dans le tableau.
+          
         for index in range(NumberOfSprites):
-            # Calcul sur quel ligne et colonne on est.
             currentLine = index%spritePerLine
             currentColumn = index//spritePerLine
             self.sprites[currentLine,currentColumn] = image.subsurface((currentLine)*spriteWidth,(currentColumn)*spriteHeight,spriteWidth,spriteHeight)
-            print("SRPTIES")
-            print(self.sprites)
+            print(self.sprites[currentLine,currentColumn])
 
-    # Retourne le sprite demandé.
-    def getSpriteAt(self, line : int, column : int) -> pygame.Surface:
-        if line > self.spritePerLine - 1 or line < 0:
+    def getSpriteAt(self, column : int, line : int) -> pygame.surface:
+        if line > self.spritePerLine - 1 | line < 0:
             raise Exception("line index not valid")
-        if column > self.spritePerColumn - 1 or column < 0:
+        if column > self.spritePerColumn - 1 | column < 0:
             raise Exception("column index not valid")
         return self.sprites[line,column]
     
