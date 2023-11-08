4df60df7321c5f7e78473b5c53592ab50d605089
estiennq <Quentin.Estienne@etu.univ-grenoble-alpes.fr>
2023-11-08 18:17:31 +0100

    delete testSpriteSheet + SpriteSheet fonctionnel

--- SpriteSheet.py
+++ SpriteSheet.py
@@ -1,8 +1,8 @@
 import pygame 
 
-class SpriteSheet():
+class SpriteSheet(object):
 
-    def __init__(self,filename : str ,spritePerLine , spritePerColumn , spriteWidth , spriteHeight,):
+    def __init__(self,filename : str, spritePerLine, spritePerColumn, spriteWidth, spriteHeight):
         image = pygame.image.load(filename)
         NumberOfSprites = spritePerLine*spritePerColumn
         self.sprites = {}
@@ -12,13 +12,12 @@ class SpriteSheet():
         for index in range(NumberOfSprites):
             currentLine = index%spritePerLine
             currentColumn = index//spritePerLine
-        self.sprites[currentLine,currentColumn] = image.subsurface((currentLine)*spriteWidth,(currentColumn)*spriteWidth,spriteWidth,spriteHeight)
-        print(self.sprites[currentLine,currentColumn])
+            self.sprites[currentLine,currentColumn] = image.subsurface((currentLine)*spriteWidth,(currentColumn)*spriteHeight,spriteWidth,spriteHeight)
 
-    def getSpriteAt(self,line : int, column : int):
-        if line > self.spritePerLine | line < 0:
+    def getSpriteAt(self, line : int, column : int) -> pygame.sprite.Sprite:
+        if line > self.spritePerLine - 1 | line < 0:
             raise Exception("line index not valid")
-        if column > self.spritePerColumn | column < 0:
+        if column > self.spritePerColumn - 1 | column < 0:
             raise Exception("column index not valid")
         return self.sprites[line,column]
     
